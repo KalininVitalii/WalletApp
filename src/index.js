c4193f07b825b72a3f3c3e06fa16524b27a5e412
KalininVitalii <105376147+KalininVitalii@users.noreply.github.com>
2023-02-14 07:28:58 +0200

    fixed

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <ThemeProvider theme={theme}>
       <Provider store={store}>
         <PersistGate persistor={persistor}>
-          <BrowserRouter basename="/wallet-reactor/">
+          <BrowserRouter basename="/WalletApp/">
             <App />
           </BrowserRouter>
         </PersistGate>
